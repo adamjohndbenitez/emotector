6f969e16378935bdd11021a2775125cd480ce92f
hamsterdam <adamjohndbenitez72891@gmail.com>
2017-01-05 17:26:47 +0800

    task[2] v2 Improve code structure.

--- server.R
+++ server.R
@@ -1,13 +1,3 @@
+shiny::shinyServer(function(input, output) {
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-
-shinyServer(function(input, output) {
-
-  
 })
--- ui.R
+++ ui.R
@@ -1,20 +1,8 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-library(shiny)
-require("Rfacebook")
-library(shinyjs)
-
 shinydashboard::dashboardPage(skin = "blue",
   shinydashboard::dashboardHeader(title = div(img(src = "E.png", width = "15%", height = "15%"), "moTector")),
   shinydashboard::dashboardSidebar(
     div(class = "facebookP", "Input Facebook page ID"),
-    sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+    shinydashboard::sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
                       label = "Search..."),
     textInput("text", "Text input:")
   ),
