9863db7396dc15850ef5263102e3abccd4faff16
maranan.ld <maranan.ld@sprobe.com>
2017-01-05 16:29:05 +0800

    Task[2] v2 Redesign using shiny dashboard.
    	- changes in ui.R.
    	- remove all functionality in server.R.
    	- add custom.css

--- server.R
+++ server.R
@@ -1,48 +1,13 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
 #
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# http://shiny.rstudio.com
 #
 
 library(shiny)
-library(Rfacebook)
-library(xlsx)
 
-#generate facebook
-function(input, output) {
-  
-  # builds a reactive expression that only invalidates 
-  # when the value of input$goButton becomes out of date 
+shinyServer(function(input, output) {
 
-  facebookPage <- eventReactive(input$generate, {
-    input$n
-    fb_page <- getPage(page=input$n, token=fb_oauth)
-    write.table(fb_page, "C:/Users/maranan.ld/Documents/MSTR/EmoTector/www/mydata.xls", sep="\t")
-  })
   
-  output$facebookPage <- renderText({
-    facebookPage()
-  })
-  
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-             quote=input$quote)
-  })
-  
- 
-}
+})
