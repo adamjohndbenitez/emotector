b8951ac270926c94d2cc35b0cd2ccef233691057
Adam John Benitez <benitez.ajd@sprobe.com>
2017-01-06 18:05:01 +0800

    [Fix] Facebook function cannot see oauth token.

--- R/server.R
+++ R/server.R
@@ -1,16 +1,11 @@
 shiny::shinyServer(function(input, output) {
+  
   ntext <- shiny::eventReactive(input$searchButton, {
-    input$searchText
-    fb_page <- Rfacebook::getPage(page=input$searchText, token=fb_oauth)
-    #fb_page <- Rfacebook::getNewsfeed(token = fb_oauth, n=10)
+    fb_page <- Rfacebook::getPage(page = input$searchText, token = fb_oauth)
   })
   
-  output$nText <- shiny::renderText({
+  output$nText <- shiny::renderPrint({
     ntext()
-  })
-  
-  output$sample <- shiny::renderText({
-    fb_page <- Rfacebook::getPage(page="indaysara", token=fb_oauth)
   })
   
 })
