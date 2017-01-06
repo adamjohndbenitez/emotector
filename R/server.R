bb831dc09359c76ec58282db160e1e89792d6eeb
hamsterdam <adamjohndbenitez72891@gmail.com>
2017-01-07 03:49:13 +0800

    Task[4] v1 Improve OAuth
        - remove fbOauth() and save() since it was generated already.
        - retain load() for token.

--- R/server.R
+++ R/server.R
@@ -1,7 +1,11 @@
 shiny::shinyServer(function(input, output) {
   
+  base::load(file = "fb_oauth")
+  
   ntext <- shiny::eventReactive(input$searchButton, {
     fb_page <- Rfacebook::getPage(page = input$searchText, token = fb_oauth)
+    post <- Rfacebook::getPost(post=fb_page$id[1], n=2000, token=fb_oauth)
+    print(fb_page$message[10])
   })
   
   output$nText <- shiny::renderPrint({
