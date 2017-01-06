2a8d3c8064a7a0485bc9984fa0b6b1b2ea4da4b9
maranan.ld <maranan.ld@sprobe.com>
2017-01-06 14:48:10 +0800

    Task[2] v3 Redesign using shiny dashboard

--- R/server.R
+++ R/server.R
@@ -1,15 +1,16 @@
 shiny::shinyServer(function(input, output) {
+  ntext <- shiny::eventReactive(input$searchButton, {
+    input$searchText
+    fb_page <- Rfacebook::getPage(page=input$searchText, token=fb_oauth)
+    #fb_page <- Rfacebook::getNewsfeed(token = fb_oauth, n=10)
+  })
+  
+  output$nText <- shiny::renderText({
+    ntext()
+  })
+  
+  output$sample <- shiny::renderText({
+    fb_page <- Rfacebook::getPage(page="indaysara", token=fb_oauth)
+  })
   
 })
-
-#' Add together two numbers.
-#'
-#' @param x A number.
-#' @param y A number.
-#' @return The sum of \code{x} and \code{y}.
-#' @examples
-#' add(1, 1)
-#' @export
-add <- function(x, y) {
- x + y
-}
--- R/ui.R
+++ R/ui.R
@@ -1,16 +1,25 @@
 shinydashboard::dashboardPage(skin = "blue",
-  shinydashboard::dashboardHeader(title = shiny::div(shiny::img(src = "E.png", width = "15%", height = "15%"), "moTector")),
+  shinydashboard::dashboardHeader(title = 
+    shiny::div(
+      shiny::img(src = "E.png", width = "15%", height = "15%"), "moTector"
+    )
+  ),
   shinydashboard::dashboardSidebar(
     shiny::div(class = "facebookP", "Input Facebook page ID"),
-    shinydashboard::sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
-                      label = "Search..."),
-    shiny::textInput("text", "Text input:")
+    shinydashboard::sidebarSearchForm(textId = "searchText", buttonId = "searchButton", label = "Search...")
+    #shiny::textInput("text", "Text input:")
   ),
   shinydashboard::dashboardBody(
+    shinydashboard::box(
+      title = "Message Generated", width = 4, solidHeader = TRUE, status = "primary", shiny::textOutput(outputId = "nText")
+    ),
+    shinydashboard::box(
+      title = "SAMPLE", width = 4, solidHeader = TRUE, status = "primary", shiny::textOutput(outputId = "sample")
+    ),
     shiny::tags$head(
       shiny::tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
-    ),
-    shiny::h1("this is from shiny header")
+    )
+    #shiny::h1("this is from shiny header")
   )
 )
 
