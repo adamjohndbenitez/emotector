d88869163f6566da0c37d44aed70d8175885b3e5
hamsterdam <adamjohndbenitez72891@gmail.com>
2017-01-09 23:05:02 +0800

    Task[6] v2 Manual input
       1. Make checkbox to manually input post on textarea.
       2. Make textarea on sidemenu, if checkbox is check then show textarea.
       3. Make a submit button to updated Post box.

--- R/server.R
+++ R/server.R
@@ -1,23 +1,35 @@
 shiny::shinyServer(function(input, output, session) {
   base::load(file = "fb_oauth")
-  
-  shiny::updateDateRangeInput(session = session, inputId = "dateRangeId", start = Sys.Date() - 10, end = Sys.Date() + 10)
-  
+
+  shiny::updateDateRangeInput(session = session, inputId = "dateRangeId", start = base::Sys.Date() - 10, end = base::Sys.Date())
+
   shiny::observeEvent(input$searchButton, {
-    listOfPosts <- Rfacebook::getPage(page = input$searchText, token = fb_oauth, n = as.numeric(input$numberOfPosts), since = input$dateRangeId[1], until = input$dateRangeId[2], verbose = FALSE)
+    listOfPosts <- Rfacebook::getPage(page = input$searchText, token = fb_oauth, n = base::as.numeric(input$numberOfPosts), since = input$dateRangeId[1], until = input$dateRangeId[2], verbose = FALSE)
 
     output$postListUIId <- shiny::renderUI({
-      shiny::selectInput(inputId = "postListId", label = "Select Post", choices = 1:length(listOfPosts$message))
+      shiny::selectInput(inputId = "postListId", label = "Select Post", choices = 1:base::length(listOfPosts$message))
     })
-    
+
     output$viewPostId <- shiny::renderText({
       listOfPosts$message[as.numeric(input$postListId)]
     })
-    
+
     output$viewPostUIId <- shiny::renderUI({
       shinydashboard::box(title = "Post", width = 12, solidHeader = TRUE, status = "primary",
-          shiny::textOutput(outputId = "viewPostId")
+        shiny::textOutput(outputId = "viewPostId")
       )
+    })
+  })
+
+  shiny::observeEvent(input$submitManualPostId, {
+    output$viewPostUIId <- shiny::renderUI({
+      shinydashboard::box(title = "Post", width = 12, solidHeader = TRUE, status = "primary",
+        shiny::textOutput(outputId = "viewPostId")
+      )
+    })
+
+    output$viewPostId <- shiny::renderText({
+      input$manualPostTextAreaId
     })
   })
 })
--- R/ui.R
+++ R/ui.R
@@ -10,12 +10,22 @@ shinydashboard::dashboardPage(skin = "blue",
     ),
     shinydashboard::sidebarSearchForm(textId = "searchText", buttonId = "searchButton", label = "Search..."),
     shinydashboard::sidebarMenu(
-      shinydashboard::menuItem("Search Settings:", icon = shiny::icon(name = "cogs", class = "fa-1x", lib = "font-awesome"),
+      shinydashboard::menuItem("Search Page ID Settings:", icon = 
+        shiny::icon(name = "cogs", class = "fa-1x", lib = "font-awesome"),
         shiny::dateRangeInput(inputId = "dateRangeId", label = "Date Range of Posts"),
-        shiny::numericInput(inputId = "numberOfPosts", label = "Number of Posts", value = 50)
+        shiny::numericInput(inputId = "numberOfPosts", label = "Number of Posts", value = 10)
       )
     ),
-    shiny::uiOutput(outputId = "postListUIId")
+    shiny::tags$br(),
+    shiny::tags$hr(),
+    shiny::uiOutput(outputId = "postListUIId"),
+    shiny::checkboxInput(inputId = "checkManualPostId", label = "Manual input post: ", value = FALSE, width = '100%'),
+    shiny::conditionalPanel(condition = "input.checkManualPostId == true", 
+      shiny::textAreaInput(inputId = "manualPostTextAreaId", label = NULL, value = "", width = '100%', height = '200px', placeholder = "Input post..."),
+      shiny::actionButton(inputId = "submitManualPostId", label = "Submit Post", icon = 
+        shiny::icon(name = "hdd-o", class = "fa-1x", lib = "font-awesome"), width = '100%'
+      )
+    )
   ),
   shinydashboard::dashboardBody(
     shiny::uiOutput(outputId = "viewPostUIId"),
