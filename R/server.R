ab4e5e4317046dbcdd7684973bb6aaaa4fce5849
hamsterdam <adamjohndbenitez72891@gmail.com>
2017-01-11 01:28:20 +0800

    Task[7] v1 Error should be handle during incorrect input facebook page id.
    	1. Using tryCatch() to handle error.
    	2. Showing notification when valid input for facebook page id.
    	3. Showing error notification when invalid input for facebook page id.
    	4. Also show link on how to ensure correct facebook page id.

--- R/server.R
+++ R/server.R
@@ -4,14 +4,24 @@ shiny::shinyServer(function(input, output, session) {
   shiny::updateDateRangeInput(session = session, inputId = "dateRangeId", start = base::Sys.Date() - 10, end = base::Sys.Date())
 
   shiny::observeEvent(input$searchButton, {
-    listOfPosts <- Rfacebook::getPage(page = input$searchText, token = fb_oauth, n = base::as.numeric(input$numberOfPosts), since = input$dateRangeId[1], until = input$dateRangeId[2], verbose = FALSE)
+    base::tryCatch(expr = {
+      listOfPosts <- Rfacebook::getPage(page = input$searchText, token = fb_oauth, n = base::as.numeric(input$numberOfPosts), since = input$dateRangeId[1], until = input$dateRangeId[2], verbose = FALSE)
 
-    output$postListUIId <- shiny::renderUI({
-      shiny::selectInput(inputId = "postListId", label = "Select Post", choices = 1:base::length(listOfPosts$message))
-    })
-
-    output$viewPostId <- shiny::renderText({
-      listOfPosts$message[as.numeric(input$postListId)]
+      output$postListUIId <- shiny::renderUI({
+        shiny::selectInput(inputId = "postListId", label = "Select Post", choices = 1:base::length(listOfPosts$message))
+      })
+  
+      output$viewPostId <- shiny::renderText({
+        listOfPosts$message[as.numeric(input$postListId)]
+      })
+      
+      shiny::showNotification(ui = "We're good to go...", duration = 5, closeButton = FALSE, type = "message", session = shiny::getDefaultReactiveDomain())
+    }, error = function(e) {
+      shiny::showNotification(ui = "The Facebook page or group ID you’re using is not correct or invalid. Click link below", action = 
+        shiny::tags$a(href = "https://smashballoon.com/custom-facebook-feed/id/", "Ensure valid facebook page ID."), duration = NULL, closeButton = TRUE, type = "error", session = shiny::getDefaultReactiveDomain())
+    }, warning = function(w) {
+      shiny::showNotification(ui = "Waring message.", duration = 5, closeButton = FALSE, type = "warning", session = shiny::getDefaultReactiveDomain())
+    }, finally = {
     })
 
     output$viewPostUIId <- shiny::renderUI({
