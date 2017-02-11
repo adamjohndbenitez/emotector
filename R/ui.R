6c9a27751e47f7219b7495ef7c05759c3a275f15
litlit <litlitmaranan91490@gmail.com>
2017-02-11 18:06:16 +0800

    [Improvements] Improve UI

--- R/ui.R
+++ R/ui.R
@@ -10,22 +10,28 @@ shinydashboard::dashboardPage(skin = "blue",
       shiny::icon(name = "facebook-f", class = "fa-1x", lib = "font-awesome"), "acebook page ID"
     ),
     shinydashboard::sidebarSearchForm(textId = "searchText", buttonId = "searchButton", label = "Search..."),
+    shiny::uiOutput(outputId = "postListUIId"),
     shinydashboard::sidebarMenu(
       shinydashboard::menuItem("Search Page ID Settings:", icon = 
         shiny::icon(name = "cogs", class = "fa-1x", lib = "font-awesome"),
         shiny::dateRangeInput(inputId = "dateRangeId", label = "Date Range of Posts"),
         shiny::numericInput(inputId = "numberOfPosts", label = "Number of Posts", value = 10),
-        shiny::numericInput(inputId = "numberOfComments", label = "Number of Posts", value = 10)
+        shiny::numericInput(inputId = "numberOfComments", label = "Number of Comments", value = 10)
       )
     ),
     shiny::tags$br(),
     shiny::tags$hr(),
-    shiny::uiOutput(outputId = "postListUIId"),
+    shiny::div(class = "facebookP", " ",
+      shiny::uiOutput(outputId = "downloadCSV")
+    ),
+    shiny::tags$hr(),
     shiny::checkboxInput(inputId = "checkManualPostId", label = "Manual input post: ", value = FALSE, width = "100%"),
     shiny::conditionalPanel(condition = "input.checkManualPostId == true", 
       shiny::textAreaInput(inputId = "manualPostTextAreaId", label = NULL, value = "", width = "100%", height = "200px", placeholder = "Input post..."),
-      shiny::actionButton(inputId = "submitManualPostId", label = "Submit Post", icon = 
-        shiny::icon(name = "hdd-o", class = "fa-1x", lib = "font-awesome"), width = "100%"
+      shiny::div(class= "submitPostBtn", " ",
+         shiny::actionButton(inputId = "submitManualPostId", label = "Submit Post", icon = 
+            shiny::icon(name = "hdd-o", class = "fa-1x", lib = "font-awesome"), width = "70%"
+         )  
       )
     )
   ),
