82e1c5d53c2c0226f3b17d6e46ecf48bc10c43d0
litlit <litlitmaranan91490@gmail.com>
2017-02-19 22:46:44 +0800

    Task[11] v2 Analyze Button - show emotion tab and graphs onclick analyze button

--- R/ui.R
+++ R/ui.R
@@ -58,112 +58,31 @@ shinydashboard::dashboardPage(skin = "blue",
     )
   ),
   shinydashboard::dashboardBody(
+    # shiny::div(
+    #   shiny::img(src = "insideout.gif", width = "100%", height = "10%")
+    # ),
     shiny::fluidRow(
       shiny::uiOutput(outputId = "viewPostUIId")
     ),
     shiny::fluidRow(
       shiny::uiOutput(outputId = "viewCommentsUIId")
     ),
+    
     shiny::fluidRow(
-    shinydashboard::tabBox(id = "tabBoxId", selected = "All", title = "Emotions", width = 12, height = NULL, side = "left",
-      shiny::tabPanel(title = "All Emotions", value = "All", icon = shiny::icon(name = "smile-o", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "allJoyBoxId", width = 3),
-          shinydashboard::valueBoxOutput(outputId = "allSadnessBoxId", width = 3),
-          shinydashboard::valueBoxOutput(outputId = "allAngerBoxId", width = 3),
-          shinydashboard::valueBoxOutput(outputId = "allDisgustBoxId", width = 3),
-          shinydashboard::valueBoxOutput(outputId = "allFearBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Joy", value = "Joy", icon = shiny::icon(name = "smile-o", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "joyBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyHighestBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyHigherBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyHighBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyNeutralBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyLowBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "joyLowestBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Sadness", value = "Sadness", icon = shiny::icon(name = "frown-o", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "sadnessBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessHighestBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessHigherBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessHighBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessNeutralBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessLowBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadnessLowestBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Anger", value = "Anger", icon = shiny::icon(name = "hand-rock-o", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "angerBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerHighestBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerHigherBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerHighBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerNeutralBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerLowBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angerLowestBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Disgust", value = "Disgust", icon = shiny::icon(name = "thumbs-o-down", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "disgustBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustHighestBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustHigherBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustHighBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustNeutralBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustLowBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "disgustLowestBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Fear", value = "Fear", icon = shiny::icon(name = "heartbeat", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "fearBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearHighestBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearHigherBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearHighBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearNeutralBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearLowBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "fearLowestBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Emojis Reaction", value = "Emojis", icon = shiny::icon(name = "facebook-square", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "loveBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "hahaBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "sadBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "angryBoxId", width = 3)
-        )
-      ),
-      shiny::tabPanel(title = "Emoticons", value = "Emoticons", icon = shiny::icon(name = "circle", class = "fa-1x", lib = "font-awesome"),
-        shiny::fluidRow(
-          shinydashboard::valueBoxOutput(outputId = "JoyEmoticonsBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "SadnessEmoticonsBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "AngerEmoticonsBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "DisgustEmoticonsBoxId", width = 3),
-          shinydashboard::infoBoxOutput(outputId = "FearEmoticonsBoxId", width = 3)
-        )
-      )
-    )
+      shiny::uiOutput(outputId = "showEmotionsBoxes")
     ),
+    
     shiny::fixedRow(
       column(width = 12, 
         fixedRow(
           column(width = 6,
-            shiny::fluidRow(
-              shinydashboard::box(title = "Summary of Weighted Emotions", width = 12, solidHeader = FALSE, status = "primary", background = NULL,
-                shiny::plotOutput("plot")
-              )
-            )
+             shiny::fluidRow(
+               shiny::uiOutput(outputId = "showSummaryWeightedEmotion")
+             )
           ),
           column(width = 6,
             shiny::fluidRow(
-              shinydashboard::box(title = "Degree of Emotions (Count-based Stacked Barplot)", width = 12, solidHeader = FALSE, status = "primary", background = NULL,
-                shiny::plotOutput("plot1")
-              )
+              shiny::uiOutput(outputId = "showDegreeOfEmotion")
             )
           )
         )
